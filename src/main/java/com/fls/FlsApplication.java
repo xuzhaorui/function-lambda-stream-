67326528aa7b6c7acbf975614ae75165a0a9f81f
许钊瑞 <xuzhaorui.yx@qq.com>
2025-01-02 16:40:20 +0800

    首次提交

---
+++ src/main/java/com/fls/FlsApplication.java
@@ -0,0 +1,13 @@
+package com.fls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fls/FlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
